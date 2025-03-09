5a4ebf11ed16904a144cc69e5899269ca0941856
bisnad <daniel.bisig@zhdk.ch>
2025-03-09 13:26:26 +0100

    fixed deprecated axis use in PoseRenderer

--- rnn/rnn_pos.py
+++ rnn/rnn_pos.py
@@ -37,7 +37,7 @@ mocap_config_file = "data/configs/COCO_config.json"
 mocap_file_path = "../../../Data/Mocap/Pose2D/Stocos/Solos"
 mocap_files = ["Stocos_Pose2D_BlumenBaile.pkl"]
 mocap_valid_frame_ranges = [ [ [ 0, 5000 ] ] ]
-mocap_sensor_ids = ["/mocap/0/joint/pos_world", "/mocap/0/joint/visibility"]
+mocap_sensor_ids = ["/mocap/0/joint/pos_world"]
 mocap_root_joint_name = "Left_Hip"
 mocap_fps = 30
 mocap_joint_dim = 2
@@ -45,27 +45,14 @@ mocap_joint_dim = 2
 """
 # Example: MMPose 3D-Pose Estimation Recording
 mocap_config_file = "data/configs/Human36M_config.json" 
-mocap_file_path = "../../../Data/Mocap/Pose3D/HannahMartin/Solos/pkl"
-mocap_files = ["HannahMartin_Pos3D_Performance.pkl"]
-mocap_valid_frame_ranges = [ [ [ 0, 3600 ] ] ]
-mocap_sensor_ids = ["/mocap/0/joint/pos3d_world", "/mocap/0/joint/visibility"]
+mocap_file_path = "../../../Data/Mocap/Pose3D/Stocos/Solos"
+mocap_files = ["Stocos_Pose3D_BlumenBaile.pkl"]
+mocap_valid_frame_ranges = [ [ [ 0, 5000 ] ] ]
+mocap_sensor_ids = ["/mocap/0/joint/pos_world"]
 mocap_root_joint_name = "Bottom_Torso"
 mocap_fps = 30
 mocap_joint_dim = 3
 """
-
-"""
-# Example: MMPose 2D-Pose Estimation Recording
-mocap_config_file = "data/configs/Halpe26_config.json" 
-mocap_file_path = "../../../Data/Mocap/Pose2D/HannahMartin/Solos/pkl"
-mocap_files = ["HannahMartin_Pos2D_Performance.pkl"]
-mocap_valid_frame_ranges = [ [ [ 0, 3600 ] ] ]
-mocap_sensor_ids = ["/mocap/0/joint/pos2d_world", "/mocap/0/joint/visibility"]
-mocap_root_joint_name = "Hip"
-mocap_fps = 30
-mocap_joint_dim = 2
-"""
-
 
 """
 Model Settings
