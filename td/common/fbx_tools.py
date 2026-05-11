3da8031afe5db869d3bc629f95e056d2c3409cdb
bisnad <daniel.bisig@zhdk.ch>
2026-05-11 11:30:32 +0200

    Update fbx_tools.py

--- td/common/fbx_tools.py
+++ td/common/fbx_tools.py
@@ -287,7 +287,7 @@ class FBX_Tools():
         
         fps = mocap_data[0].motion_frame_rate
         
-        if fps.is_integer() == True:
+        if float(fps).is_integer():
             fps = int(fps)
         
         fps_string = "{}".format(fps)
